8d44d9313f706a77ede98e4b796458f7eb4e8b2c
Tusharzen <tushargupta843@gmail.com>
2022-11-29 12:22:37 +0530

    Add solved code

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,24 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    public Boolean cheese = false ;
+    public Boolean topping =false ;
+    public Boolean carrybag = false ;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+
+        if(this.isVeg==true)
+        {
+            this.price = 300 ;
+            this.bill = "Base Price Of The Pizza: 300\n" ;
+        }
+        else
+        {
+            this.price=400 ;
+            this.bill = "Base Price Of The Pizza: 400\n" ;
+        }
     }
 
     public int getPrice(){
@@ -16,19 +30,45 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(cheese==false)
+        price+= 80;
+        cheese = true ;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(topping==false)
+        {
+            if(isVeg ==true)
+            price += 70 ;
+            else
+            price += 120 ;
+        }
+        topping = true ;
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(carrybag==false)
+        price += 20 ;
+        carrybag = true ;
     }
 
     public String getBill(){
         // your code goes here
+        if(cheese==true)
+        bill += "Extra Cheese Added: 80\n" ;
+
+        if(topping == true && isVeg==true)
+        bill+= "Extra Toppings Added: 70\n";
+
+        if(topping==true && isVeg==false)
+        bill+= "Extra Toppings Added: 120\n";
+
+        if(carrybag==true)
+        bill+= "Paperbag Added: 20\n";
+        
+        bill+= "Total Price: "+price ;
+
         return this.bill;
     }
 }
